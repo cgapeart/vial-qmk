0345ec65d589fd9bba8e9752fa08a3efe38d894b
Colin Peart <Colin.Peart@mobiledatatech.com>
2024-07-05 17:01:22 -0600

    Stuff basically setup

--- keyboards/6x6_macropad/keymaps/vial/config.h
+++ keyboards/6x6_macropad/keymaps/vial/config.h
@@ -2,3 +2,4 @@
 #define VIAL_KEYBOARD_UID {0xD9, 0x6F, 0x13, 0xE1, 0xA2, 0x30, 0x74, 0xED}
 #define VIAL_UNLOCK_COMBO_ROWS { 1, 2 }
 #define VIAL_UNLOCK_COMBO_COLS { 1, 2 }
+#define TAPPING_TOGGLE 2
--- keyboards/handwired/dactyl_manuform/7x7_cp/keymaps/vial/config.h
+++ keyboards/handwired/dactyl_manuform/7x7_cp/keymaps/vial/config.h
@@ -2,3 +2,4 @@
 #define VIAL_KEYBOARD_UID {0x53, 0x7E, 0x3C, 0x73, 0x81, 0x7F, 0x34, 0x2D}
 #define VIAL_UNLOCK_COMBO_ROWS { 1, 2 }
 #define VIAL_UNLOCK_COMBO_COLS { 1, 2 }
+#define TAPPING_TOGGLE 2
