36a820462881f0efa8fbbf94cc23486d5df27548
Colin Peart <Colin.Peart@mobiledatatech.com>
2024-07-05 15:02:18 -0600

    port ultipad, adding minipc :

---
+++ keyboards/ultipad/config.h
@@ -0,0 +1,9 @@
+
+
+#pragma once
+
+
+/* LED layer indicators */
+#define LAYER_INDICATOR_LED_0 B6
+#define LAYER_INDICATOR_LED_1 B2
+
---
+++ keyboards/ultipad/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+#define TAPPING_TOGGLE 2
+#define VIAL_KEYBOARD_UID {0x79, 0x18, 0xC7, 0x8B, 0x40, 0x55, 0x5E, 0xF8}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 1 }
