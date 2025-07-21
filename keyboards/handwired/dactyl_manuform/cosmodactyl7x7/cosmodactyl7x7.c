a97eadb77c2c4e0527c521f0400335dbd2928129
Colin Peart <Colin.Peart@mobiledatatech.com>
2025-07-21 08:49:49 -0600

    started adding a basis for colouring keys during layer switches

--- keyboards/handwired/dactyl_manuform/cosmodactyl7x7/cosmodactyl7x7.c
+++ keyboards/handwired/dactyl_manuform/cosmodactyl7x7/cosmodactyl7x7.c
@@ -7,27 +7,153 @@
 bool isOledGood = false;
 int16_t sjsx = 0, sjsy = 0;
 bool sjsb = 0;
+#define LED_FLAG_LYR_1 0x10
+#define LED_FLAG_LYR_2 0x20
+
 // LED mappings
-led_config_t g_led_config = {{// Key Matrix Postion to LED index
-                              {0, 5, 10, 15, 21, 27, 32},
-                              {1, 6, 11, 16, 22, 28, 33},
-                              {2, 7, 12, 17, 23, 29, 34},
-                              {3, 8, 13, 18, 24, 30, 35},
-                              {4, 9, 14, 19, 25, 31, 36},
-                              {NO_LED, NO_LED, NO_LED, 20, 26, NO_LED, NO_LED},
-                              {37, 38, 39, 40, 41, 42, NO_LED},
-                              {43, 48, 53, 58, 64, 70, 75},
-                              {44, 49, 54, 59, 65, 71, 76},
-                              {45, 50, 55, 60, 66, 72, 77},
-                              {46, 51, 56, 61, 67, 73, 78},
-                              {47, 52, 57, 62, 68, 74, 79},
-                              {NO_LED, NO_LED, NO_LED, 63, 69, NO_LED, NO_LED},
-                              {80, 81, 82, 83, 84, 85, NO_LED}},
-                             {// LED Index to physical position
-                              {0, 2}, {0, 9}, {1, 17}, {1, 25}, {1, 33}, {11, 1}, {12, 9}, {12, 17}, {13, 25}, {13, 33}, {22, 1}, {23, 8}, {24, 16}, {24, 25}, {24, 33}, {34, 1}, {35, 8}, {36, 16}, {36, 25}, {36, 33}, {36, 41}, {46, 0}, {47, 7}, {47, 15}, {48, 23}, {48, 32}, {48, 40}, {57, 1}, {57, 8}, {58, 16}, {58, 25}, {58, 33}, {68, 1}, {68, 9}, {69, 17}, {69, 25}, {69, 33}, {64, 47}, {73, 57}, {76, 64}, {77, 47}, {85, 55}, {88, 61}, {224, 2}, {223, 9}, {222, 17}, {222, 25}, {222, 33}, {212, 1}, {211, 9}, {211, 17}, {210, 25}, {210, 33}, {201, 1}, {200, 8}, {199, 16}, {199, 25}, {199, 33}, {188, 1}, {188, 8}, {187, 16}, {187, 25}, {187, 33}, {187, 41}, {177, 0}, {176, 7}, {176, 15}, {175, 23}, {175, 32}, {175, 40}, {166, 1}, {166, 8}, {165, 16}, {165, 25}, {165, 33}, {155, 1}, {155, 9}, {154, 17}, {154, 25}, {154, 33}, {159, 47}, {150, 57}, {147, 64}, {146, 47}, {138, 55}, {135, 61}},
-                             {// key flags
-                              LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
-                              LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT}};
+led_config_t g_led_config =
+{
+    {// Key Matrix Postion to LED index
+        {0, 5, 10, 15, 21, 27, 32},
+        {1, 6, 11, 16, 22, 28, 33},
+        {2, 7, 12, 17, 23, 29, 34},
+        {3, 8, 13, 18, 24, 30, 35},
+        {4, 9, 14, 19, 25, 31, 36},
+        {NO_LED, NO_LED, NO_LED, 20, 26, NO_LED, NO_LED},
+        {37, 38, 39, 40, 41, 42, NO_LED},
+        {43, 48, 53, 58, 64, 70, 75},
+        {44, 49, 54, 59, 65, 71, 76},
+        {45, 50, 55, 60, 66, 72, 77},
+        {46, 51, 56, 61, 67, 73, 78},
+        {47, 52, 57, 62, 68, 74, 79},
+        {NO_LED, NO_LED, NO_LED, 63, 69, NO_LED, NO_LED},
+        {80, 81, 82, 83, 84, 85, NO_LED}
+    },
+    {// LED Index to physical position
+       {0, 2}, {0, 9}, {1, 17}, {1, 25}, {1, 33},
+        {11, 1}, {12, 9}, {12, 17}, {13, 25}, {13, 33},
+        {22, 1}, {23, 8}, {24, 16}, {24, 25}, {24, 33},
+        {34, 1}, {35, 8}, {36, 16}, {36, 25}, {36, 33},
+        {36, 41}, {46, 0}, {47, 7}, {47, 15}, {48, 23},
+        {48, 32}, {48, 40}, {57, 1}, {57, 8}, {58, 16},
+        {58, 25}, {58, 33}, {68, 1}, {68, 9}, {69, 17},
+        {69, 25}, {69, 33}, {64, 47}, {73, 57}, {76, 64},
+        {77, 47}, {85, 55}, {88, 61}, {224, 2}, {223, 9},
+        {222, 17}, {222, 25}, {222, 33}, {212, 1}, {211, 9},
+        {211, 17}, {210, 25}, {210, 33}, {201, 1}, {200, 8},
+        {199, 16}, {199, 25}, {199, 33}, {188, 1}, {188, 8},
+        {187, 16}, {187, 25}, {187, 33}, {187, 41}, {177, 0},
+        {176, 7}, {176, 15}, {175, 23}, {175, 32}, {175, 40},
+        {166, 1}, {166, 8}, {165, 16}, {165, 25}, {165, 33},
+        {155, 1}, {155, 9}, {154, 17}, {154, 25}, {154, 33},
+        {159, 47}, {150, 57}, {147, 64}, {146, 47}, {138, 55}, {135, 61}
+    },
+    {// key flags
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1| LED_FLAG_LYR_2,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_2,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1| LED_FLAG_LYR_2,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1| LED_FLAG_LYR_2,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1| LED_FLAG_LYR_2,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT| LED_FLAG_LYR_2,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT| LED_FLAG_MODIFIER,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT| LED_FLAG_LYR_2,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT| LED_FLAG_LYR_2,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT| LED_FLAG_MODIFIER,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1 | LED_FLAG_MODIFIER,
+        LED_FLAG_KEYLIGHT| LED_FLAG_LYR_2,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT |LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT |LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT| LED_FLAG_LYR_2,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT| LED_FLAG_LYR_2,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT| LED_FLAG_LYR_2,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT| LED_FLAG_LYR_2,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1 | LED_FLAG_LYR_2,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT | LED_FLAG_LYR_1,
+        LED_FLAG_KEYLIGHT| LED_FLAG_LYR_2,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT| LED_FLAG_MODIFIER,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT| LED_FLAG_MODIFIER,
+        LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER
+    }
+};
+
+typedef union {
+    uint8_t raw[EECONFIG_USER_DATA_SIZE];
+    struct {
+        int16_t x_offset_left;
+        int16_t y_offset_left;
+        int16_t x_offset_right;
+        int16_t y_offset_right;
+    };
+} user_config_t;
+user_config_t user_config;
+
+enum customKeyCodes
+{
+    U_CENTER_STICKS = SAFE_RANGE
+};
+bool centerSticksTrigger = false;
 
 void keyboard_post_init_kb(void)
 {
@@ -70,7 +196,7 @@ bool render_status(void)
     {
         uint8_t button0 =   (joystick_state.buttons[0] & 1) ;
         uint8_t button1 = (joystick_state.buttons[0] & 2) >> 1;
-        sprintf(buffer, "\n%c %5d,%5d,%1hd\n   %5d,%5d,%1hd",
+        sprintf(buffer, "\n%c %5d,%5d,%1hd\n  %5d,%5d,%1hd",
             is_transport_connected()? '+':'-',
             joystick_state.axes[0],joystick_state.axes[1],button0,
             joystick_state.axes[2],joystick_state.axes[3],button1);
@@ -152,7 +278,21 @@ typedef struct _joy_slave_to_master_t
 
 joystick_config_t joystick_axes[JOYSTICK_AXIS_COUNT] = {JOYSTICK_AXIS_VIRTUAL, JOYSTICK_AXIS_VIRTUAL, JOYSTICK_AXIS_VIRTUAL, JOYSTICK_AXIS_VIRTUAL};
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record)
+{
+    if(record->event.pressed)
+    {
+        switch(keycode)
+        {
+            case U_CENTER_STICKS:
+            // Offset the stick positions to treat them as centered on next joystick read., save to EEPROM
+                centerSticksTrigger = true;
+                return false;
+        }
+    }
 
+    return true; // Let the keyboard process the record
+}
 void joystick_sync_slave_handler(uint8_t in_buflen, const void *in_data, uint8_t out_buflen, void *out_data)
 {
     master_to_slave_t *m2s = (master_to_slave_t *)in_data;
@@ -172,12 +312,25 @@ void keyboard_post_init_user(void)
 
     printf("joystick input initialized\n");
     transaction_register_rpc(JOYSTICK_SYNC, joystick_sync_slave_handler);
+
+    eeconfig_read_user_datablock(user_config.raw,0,EECONFIG_USER_DATA_SIZE);
+}
+
+void eeconfig_init_user()
+{
+    user_config.x_offset_left = 0;
+    user_config.y_offset_left = 0;
+    user_config.x_offset_right = 0;
+    user_config.y_offset_right = 0;
+    eeconfig_update_user_datablock(user_config.raw,0, EECONFIG_USER_DATA_SIZE);
 }
 
 long map(long x, long in_min, long in_max, long out_min, long out_max)
 {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
+
 
 void housekeeping_task_user(void) {
     if (!isOledGood) {
@@ -197,12 +350,18 @@ void housekeeping_task_user(void) {
         {
             last_sync = timer_read32();
 
-            mx = analogReadPin(JS_X_PIN);
-            my = analogReadPin(JS_Y_PIN);
+            mx = map(analogReadPin(JS_X_PIN),0,1024,-512,512);
+            my = map(analogReadPin(JS_Y_PIN),0,1024,512,-512);
             mb = !gpio_read_pin(JS_B_PIN);
 
-            joystick_set_axis(0, map(mx,0,1024,-512,512));
-            joystick_set_axis(1, map(my,0,1024,512,-512));
+            if(centerSticksTrigger)
+            {
+                user_config.x_offset_left = mx;
+                user_config.y_offset_left = my;
+            }
+
+            joystick_set_axis(0, mx-user_config.x_offset_left);
+            joystick_set_axis(1, my-user_config.y_offset_left);
 
             if (mb)
             {
@@ -217,12 +376,18 @@ void housekeeping_task_user(void) {
             slave_to_master_t s2m = {0, 0, 0, 0};
             if (transaction_rpc_exec(JOYSTICK_SYNC, sizeof(master_to_slave_t), &m2s, sizeof(slave_to_master_t), &s2m))
             {
-                sx = s2m.x;
-                sy = s2m.y;
+                sx = map(s2m.x,0,1024,512,-512);
+                sy = map(s2m.y,0,1024,-512,512);
                 sb = s2m.b;
 
-                joystick_set_axis(2, map(sx,0,1024,512,-512));
-                joystick_set_axis(3, map(sy,0,1024,-512,512));
+                if(centerSticksTrigger)
+                {
+                    user_config.x_offset_right = sx;
+                    user_config.y_offset_right = sy;
+                }
+
+                joystick_set_axis(2, sx - user_config.x_offset_right);
+                joystick_set_axis(3, sy - user_config.y_offset_right);
 
                 if (sb)
                 {
@@ -237,6 +402,12 @@ void housekeeping_task_user(void) {
             else
             {
                 dprintf("Remote RPC failed\n");
+            }
+
+            if(centerSticksTrigger)
+            {
+                centerSticksTrigger = false;
+                eeconfig_update_user_datablock(user_config.raw,0,EECONFIG_USER_DATA_SIZE);
             }
         }
     }
